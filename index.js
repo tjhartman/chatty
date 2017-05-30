e7021daa6259039881664db84258917a6a5d3f5f
T.J. Hartman <tjhartman@TJs-MacBook-Pro.local>
2017-05-30 15:15:29 -0600

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+const express = require('express');
+const bodyParser = require('body-parser');
+var app = express()
+
+app.use(express.static('assets'))
+app.use(bodyParser.json())
+var messages = [];
+
+app.get('/messages', function(req, res, next){
+    res.status(200).json({ messages: messages})
+});
+
+app.post('/messages', function(req, res, next){
+    messages.push({ message: req.body.message, time: new Date() });
+    res.status(200).json({ messages: messages});
+});
+
+app.listen(3000)
